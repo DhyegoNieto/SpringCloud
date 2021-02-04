972868a4abf56896e6f758b14d8a12264078ef2c
Diego Nieto <dhyego.nieto@gmail.com>
2021-02-04 17:35:55 -0500

    [Course 1: Creating Eureka Server + service + client]

---
+++ client/src/test/java/io/diego/nieto/Client4CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.diego.nieto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Client4CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
